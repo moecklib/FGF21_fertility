5034633ef5d8f30c8b78a278b7eb8afc4f645b7a
Beat Moeckli <Beat.Moeckli@etu.unige.ch>
2022-06-29 09:45:11 +0200

    Outsourced the functions to a separate file and source them all together.

--- code/Fig4_ReproOrgans.R
+++ code/Fig4_ReproOrgans.R
@@ -18,70 +18,16 @@ FGF_21<-read.csv("data/FGF21_Fertility.csv")%>%
 
 FGF21_Repro<-FGF_21[1:22, c("group", "MatureCL", "MatureFoll", "EndometrialThickness")]
 
-#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
-#Plot structure & functions####
-#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#Define colors & source functions####
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
 #Theme of colours to be used throughout the manuscript. Colors chosen with Viz Palette and colorgorical
 mycolors_fill<-c("#cccccc","#FFE37E","#d94f60")
 mycolors_stroke<-c("#9B9B9B","#FFB14E","#A21736")
 
-#Theme publication function
-theme_Publication <- function(base_size=16, base_family="sans") {
-  library(grid)
-  library(ggthemes)
-  (theme_foundation(base_size=base_size, base_family=base_family)
-    + theme(plot.title = element_text(face = "bold",
-                                      size = rel(1.2), hjust = 0.5, margin = margin(0,0,20,0)),
-            text = element_text(),
-            panel.background = element_rect(colour = NA),
-            plot.background = element_rect(colour = NA),
-            panel.border = element_rect(colour = NA),
-            axis.title = element_text(face = "bold",size = rel(1)),
-            axis.title.y = element_text(angle=90,vjust =2),
-            axis.title.x = element_text(vjust = -0.2),
-            axis.text = element_text(), 
-            axis.line.x = element_line(colour="black"),
-            axis.line.y = element_line(colour="black"),
-            axis.ticks = element_line(),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            legend.key = element_rect(colour = NA),
-            legend.position = "bottom",
-            legend.direction = "horizontal",
-            legend.box = "vetical",
-            legend.key.size= unit(0.5, "cm"),
-            #legend.margin = unit(0, "cm"),
-            legend.title = element_text(face="italic"),
-            plot.margin=unit(c(10,5,5,5),"mm"),
-            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
-            strip.text = element_text(face="bold")
-    ))
-}
-
-#Function to create the standard boxplot, returns list element
-boxplot_FGF21<-function(nudge=0.6,
-                        size=0.8,
-                        textsize=5){
-  list(
-    geom_boxplot(size=1),
-    geom_point(),
-    scale_fill_manual(values=mycolors_fill),
-    scale_color_manual(values=mycolors_stroke),
-    geom_signif(comparisons = list(c("FGF21", "HFD"),
-                                   c("ND", "HFD")),
-                color="black",
-                size=size,
-                textsize = textsize),
-    geom_signif(comparisons = list(c("FGF21", "ND")), 
-                position = position_nudge(y=nudge),
-                color="black",
-                size=size,
-                textsize = textsize),
-    theme_Publication(base_size=22),
-    scale_y_continuous(expand = expansion(mult = c(0, 0.15)))
-  )
-}
+#Source the graph functions
+source("code/Functions.R")
 
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 #Plots for figure S3 (All qPCR results combined####
