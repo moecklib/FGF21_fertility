9ffb69fe75992698621d1fded6aab42cf26192da
Beat Moeckli <Beat.Moeckli@etu.unige.ch>
2022-07-04 11:18:12 +0200

    Adapted the graphs with new color scheme and slightly changed scale.

--- code/Fig4_ReproOrgans.R
+++ code/Fig4_ReproOrgans.R
@@ -12,6 +12,9 @@ lapply(c("tidyverse", "RColorBrewer", "data.table", "colorspace", "readxl",
 #Raw results import and annotation####
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
 
+FGF21_Repro<-read.csv("data/FGF21_Evaluation.csv")%>%
+  mutate(Group=factor(x=Group, levels = c("ND", "HFD", "FGF21")))
+
 #Import of raw result file
 FGF_21<-read.csv("data/FGF21_Fertility.csv")%>%
   mutate(group=factor(x=group, levels = c("ND", "HFD", "FGF21")))
@@ -22,9 +25,9 @@ FGF21_Repro<-FGF_21[1:22, c("group", "MatureCL", "MatureFoll", "EndometrialThick
 #Define colors & source functions####
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
-#Theme of colours to be used throughout the manuscript. Colors chosen with Viz Palette and colorgorical
-mycolors_fill<-c("#cccccc","#FFE37E","#d94f60")
-mycolors_stroke<-c("#9B9B9B","#FFB14E","#A21736")
+#Theme of colors, used RColorBrewer, display.brewer.pal(12, "Paired")
+mycolors_fill<-c("#FFFFFF", RColorBrewer::brewer.pal(8, "Paired")[c(1,5)])
+mycolors_stroke<-c("#000000", RColorBrewer::brewer.pal(8, "Paired")[c(2,6)])
 
 #Source the graph functions
 source("code/Functions.R")
@@ -33,17 +36,53 @@ source("code/Functions.R")
 #Plots for figure S3 (All qPCR results combined####
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
+#Number of Corpus Luteus
 FGF21_Repro %>% 
-  ggplot(aes(x=group, y=MatureCL, fill=group, color=group))+
+  ggplot(aes(x=Group, fill=Group, color=Group,
+             y=MatureCL))+
   boxplot_FGF21()+
   labs(y= "Mature Corpus Luteus", x=NULL)
+save_plot("NbCorpousLuteus")
 
+#Mature Follicules
 FGF21_Repro %>% 
-  ggplot(aes(x=group, y=MatureFoll, fill=group, color=group))+
+  ggplot(aes(x=Group, fill=Group, color=Group,
+             y=MatureFoll))+
   boxplot_FGF21(nudge = 0.4)+
   labs(y= "Mature Follicules", x=NULL)
+save_plot("NbFolicules", folder="Fig4")
 
-FGF21_Repro %>% 
-  ggplot(aes(x=group, y=EndometrialThickness, fill=group, color=group))+
-  boxplot_FGF21(nudge = 0.4)+
+#Endometrial thickness
+FGF21_Repro %>% mutate(Endomet_Thick_mm=EndometrialThickness/1E3)%>%  #Obtain the ET in mm
+  ggplot(aes(x=Group, fill=Group, color=Group,
+             y=Endomet_Thick_mm))+
+  boxplot_FGF21(nudge = 0.06)+
+  labs(y= "Endometrial thickness [mm]", x=NULL)
+save_plot("EndometThickness", folder="Fig4")
+
+#Surface of corpus luteus
+FGF21_Repro %>% mutate(Surface_CL_mm2=Surface_CL/1E6)%>%  #Obtain the CL surface in mm2 
+  ggplot(aes(x=Group, fill=Group, color=Group,
+             y=Surface_CL_mm2))+
+  boxplot_FGF21(nudge = 0.6)+
+  labs(y= "Surface CL [mm2]", x=NULL)
+save_plot("SurfaceCL")
+
+#This 
+CL_surface<- read.csv("data/measurements.csv")%>%
+  rename(Image=Image, Area=Area.Âµm.2)%>%
+  filter(!is.na(Area))%>%
+  group_by(Image)%>%
+  summarise(Count=n(), Area_CL=sum(Area), MeanArea_CL=mean(Area))%>%
+  mutate(Histo_ID=sub(pattern = ".czi.*", "", x=Image))%>%
+  left_join(FGF21_Repro[,c("Histo_ID", "Group")])
+
+CL_surface %>% 
+  ggplot(aes(x=Group, fill=Group, color=Group,
+             y=MeanArea_CL))+
+  boxplot_FGF21(nudge = 0.08E6)+
   labs(y= "Endometrial thickness [μm]", x=NULL)
+
+table(FGF21_Repro$Group)
+
+
